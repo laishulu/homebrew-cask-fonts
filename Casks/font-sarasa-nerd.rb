0c0d3f516714e9408fed8af08da9de834f8f063c
Mao <code@mao.to>
2020-09-28 23:36:23 +0800

    use double quote

--- Casks/font-sarasa-nerd.rb
+++ Casks/font-sarasa-nerd.rb
@@ -1,19 +1,19 @@
-cask 'font-sarasa-nerd' do
-  version '2.0.0'
-  sha256 '0cbc3f6904049d8190008bea753b989b43ccd0cb73696e7f3600bfd1f54222af'
+cask "font-sarasa-nerd" do
+  version "2.0.0"
+  sha256 "0cbc3f6904049d8190008bea753b989b43ccd0cb73696e7f3600bfd1f54222af"
 
   url "https://github.com/laishulu/Sarasa-Mono-SC-Nerd/archive/v#{version}.zip"
-  name 'Sarasa Mono SC Nerd font'
-  homepage 'https://github.com/laishulu/Sarasa-Mono-SC-Nerd'
+  name "Sarasa Mono SC Nerd font"
+  homepage "https://github.com/laishulu/Sarasa-Mono-SC-Nerd"
 
-  font 'sarasa-mono-sc-nerd-bold.ttf'
-  font 'sarasa-mono-sc-nerd-bolditalic.ttf'
-  font 'sarasa-mono-sc-nerd-extralight.ttf'
-  font 'sarasa-mono-sc-nerd-extralightitalic.ttf'
-  font 'sarasa-mono-sc-nerd-italic.ttf'
-  font 'sarasa-mono-sc-nerd-light.ttf'
-  font 'sarasa-mono-sc-nerd-lightitalic.ttf'
-  font 'sarasa-mono-sc-nerd-regular.ttf'
-  font 'sarasa-mono-sc-nerd-semibold.ttf'
-  font 'sarasa-mono-sc-nerd-semibolditalic.ttf'
+  font "sarasa-mono-sc-nerd-bold.ttf"
+  font "sarasa-mono-sc-nerd-bolditalic.ttf"
+  font "sarasa-mono-sc-nerd-extralight.ttf"
+  font "sarasa-mono-sc-nerd-extralightitalic.ttf"
+  font "sarasa-mono-sc-nerd-italic.ttf"
+  font "sarasa-mono-sc-nerd-light.ttf"
+  font "sarasa-mono-sc-nerd-lightitalic.ttf"
+  font "sarasa-mono-sc-nerd-regular.ttf"
+  font "sarasa-mono-sc-nerd-semibold.ttf"
+  font "sarasa-mono-sc-nerd-semibolditalic.ttf"
 end
