f73438cf2d6f1b3a3bba9278cbe32969f543a8d4
Mao <code@mao.to>
2020-09-28 10:33:01 +0800

    add sarasa nerd font

---
+++ Formula/sarasa-nerd.rb
@@ -0,0 +1,20 @@
+class SarasaNerd < Formula
+  desc "Sarasa Mono SC Nerd font"
+  homepage "https://github.com/laishulu/Sarasa-Mono-SC-Nerd"
+  url "https://github.com/laishulu/Sarasa-Mono-SC-Nerd/archive/v2.0.0.tar.gz"
+  sha256 "043ed502f4b77b20c90e94bc6fb75b9f"
+  head "https://github.com/laishulu/Sarasa-Mono-SC-Nerd/archive/master.zip"
+
+  depends_on "fontconfig"
+
+  def install
+    system "swiftc", "macism.swift"
+    bin.install "macism"
+    cp -rvf "*.ttf" ~/Library/Fonts
+    fc-cache -f -v ~/Library/Fonts
+  end
+
+  test do
+    system "true"
+  end
+end
