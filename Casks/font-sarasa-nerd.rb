bebb8479d47134ffb14fcbaa30d8da19317dc5dc
Mao <code@mao.to>
2020-09-28 22:59:27 +0800

    use zip instead of tar.gz

--- Casks/font-sarasa-nerd.rb
+++ Casks/font-sarasa-nerd.rb
@@ -1,8 +1,8 @@
 cask 'font-sarasa-nerd' do
   version '2.0.0'
-  sha256 '210bc043eee6d4eaa2a702077577c3ebebea076f6a3a9e9dfe97278b5591019d'
+  sha256 '0cbc3f6904049d8190008bea753b989b43ccd0cb73696e7f3600bfd1f54222af'
 
-  url "https://github.com/laishulu/Sarasa-Mono-SC-Nerd/archive/v#{version}.tar.gz"
+  url "https://github.com/laishulu/Sarasa-Mono-SC-Nerd/archive/v#{version}.zip"
   name 'Sarasa Mono SC Nerd font'
   homepage 'https://github.com/laishulu/Sarasa-Mono-SC-Nerd'
 
