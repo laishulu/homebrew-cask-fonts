013057d11b9a1d40ad0a107a1e812d709fc2e754
Mao <code@mao.to>
2021-12-04 23:04:18 +0800

    update

--- Casks/font-sarasa-nerd.rb
+++ Casks/font-sarasa-nerd.rb
@@ -1,6 +1,6 @@
 cask "font-sarasa-nerd" do
-  version "2.0.0"
-  sha256 "0cbc3f6904049d8190008bea753b989b43ccd0cb73696e7f3600bfd1f54222af"
+  version "2.1.0"
+  sha256 "ff46ce5070440e85b23a0326491bc902450f77d3b9f0c15e2e2ebf9feb607e91"
 
   url "https://github.com/laishulu/Sarasa-Mono-SC-Nerd/archive/v#{version}.zip"
   name "Sarasa Mono SC Nerd font"
