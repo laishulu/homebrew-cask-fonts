104fa4e69ad4d2c3678e57c90349cb85f73470cf
Mao <code@mao.to>
2023-02-15 00:52:41 +0800

    start with big version

--- Casks/font-sarasa-nerd.rb
+++ Casks/font-sarasa-nerd.rb
@@ -1,5 +1,5 @@
 cask "font-sarasa-nerd" do
-  version "1.0.0"
+  version "4.0.0"
   sha256 "f44b8e945583056c4ce3786672dcd839628a131486f55624ae828e8ed4692963"
 
   url "https://github.com/laishulu/Sarasa-Mono-SC-Nerd/archive/v#{version}/sarasa-mono-sc-nerd.zip"
