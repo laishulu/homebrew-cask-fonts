b11c0051091a1664e3ce04d6428932ba07d73e80
Mao <code@mao.to>
2023-03-22 21:13:39 +0800

    new release

--- Casks/font-sarasa-nerd.rb
+++ Casks/font-sarasa-nerd.rb
@@ -1,6 +1,6 @@
 cask "font-sarasa-nerd" do
   version "1.1.0"
-  sha256 "9decc80878f6fed511dd2d8fa0105c992cc009f3fbb83b41a3dc39e5cf2d3a1d"
+  sha256 "ffc06d50023b2c3f0904296a140405b3626aba16fb1ac168cefc87bdb90133be"
 
   url "https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/v#{version}/sarasa-term-sc-nerd.ttc.tar.gz"
   name "Sarasa Term SC Nerd font"
